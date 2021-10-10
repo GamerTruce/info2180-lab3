2abeddb49fa201f227e50e310dccb64afacb6bef
GamerTruce <maleikmiller@gmail.com>
2021-10-09 23:51:23 -0500

    Update tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,32 @@
-document.addEventListener('DOMContentLoaded', (event) => 
+document.addEventListener('DOMContentLoaded', (event) => {
+    
+    
+     
+     // Layout Board
+     
+    var board = document.getElementById("board").children;
+                 for(let s=0; s < board.length; s++)
+            {
+                const squares = board[s];
+        squares.setAttribute("class", "square");
+            }
+
+    // Ad X or O
+    
+
+    
+        
+
+    
+
+     
+    
 
 
 
 
-
-
+      
+     
 
 
 });

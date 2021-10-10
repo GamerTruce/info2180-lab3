b6fc71c3a562f37c391314605ab16f4005c3b428
GamerTruce <maleikmiller@gmail.com>
2021-10-10 02:57:32 -0500

    Add X or O when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+     
+    var playerTurn = document.getElementById("status");
+        
+    var noWin = true;
+    var count = 1;
+    var squaresArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     
     
      
@@ -9,16 +15,38 @@ document.addEventListener('DOMContentLoaded', (event) => {
             {
                 const squares = board[s];
         squares.setAttribute("class", "square");
+
+
+            
+
+    // Add X or O when Clicked
+    squares.addEventListener("click", function() {
+        if (squares.textContent== "" && noWin == true ) {
+
+            if (count % 2 == 0) {
+                squares.textContent = "O";
+                squares.classList.add("square", "O");
+                squaresArray[s] = "O";
+                playerTurn.textContent = ("Player X turn");
+
+            } 
+            
+            else 
+            {
+                squares.textContent = "X";
+                squares.classList.add("square", "X");
+                squaresArray[s] = "X";
+                playerTurn.textContent = ('Player O Turn');
             }
+            
+        }
 
-    // Ad X or O
+
+    });
+
     
-
-    
-        
-
-    
-
+    }
+            
      
     
 
@@ -30,3 +58,5 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 
 });
+
+

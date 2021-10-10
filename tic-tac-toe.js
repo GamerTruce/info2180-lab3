1fb36f2020b131530b84a641b48acc34c7022944
GamerTruce <maleikmiller@gmail.com>
2021-10-10 15:04:38 -0500

    Check for winner

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', (event) => {
      
     var playerTurn = document.getElementById("status");
-        
+    var WinningTiles = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [1, 4, 7], 
+                         [0, 3, 6], [2, 5, 8], [0, 4, 8], [2, 4, 6] 
+                       ]; 
     var noWin = true;
     var count = 1;
     var squaresArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
@@ -38,7 +40,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 squaresArray[s] = "X";
                 playerTurn.textContent = ('Player O Turn');
             }
-            
+            winner= winnerCheck(playerTurn,squaresArray);
+            count++;
         }
 
 
@@ -55,13 +58,32 @@ document.addEventListener('DOMContentLoaded', (event) => {
             });
 
 
-
+    
+        
+            
 
 
 
     }
             
-     
+     // Check For  Winner
+     function winnerCheck (playerTurn,squaresArray){
+        for (let i = 0; i < WinningTiles.length; i++) {
+            var winningRow = WinningTiles[i];
+            var r1 = winningRow[0];
+            var r2 = winningRow[1];
+            var r3 = winningRow[2];
+    
+            if (squaresArray[r1] == squaresArray[r2] && squaresArray[r2] == squaresArray[r3]) 
+            {
+                playerTurn.textContent = ('Congratulations! ' + squaresArray[r1] + ' is the Winner!');
+                return true;
+            }
+        }
+    
+        return false;
+    
+    }
     
 
 
